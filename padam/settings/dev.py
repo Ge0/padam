731e1e23dc0baadb31b17ad8978cda08356f071f
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-09-06 18:21:32 +0200

    No flake8 on settings

--- padam/settings/dev.py
+++ padam/settings/dev.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 from .base import *
 
 DEBUG = True
@@ -5,3 +6,4 @@ DEBUG = True
 # This key is only intended for development purposed and has nothing
 # that secret:
 SECRET_KEY = 'SECRET_KEY_DEV'
+GOOGLEMAP_KEY = os.environ.get('PADAM_DJANGO_GOOGLEMAP_KEY')
